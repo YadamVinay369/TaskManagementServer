2debb583ea585ec286e4bfcc5bb8420c5850ac4b
YadamVinay369 <vinay.y21@iiits.in>
2025-01-14 17:20:45 +0530

    resolved the error in database connection

--- config/connectDB.js
+++ config/connectDB.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.mongoURL);
+    await mongoose.connect(process.env.MongoURL);
     console.log("Connected to MongoDB");
   } catch (error) {
     console.log("Error connecting Database: ", error.message);
